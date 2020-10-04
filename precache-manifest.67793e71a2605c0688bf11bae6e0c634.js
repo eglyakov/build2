b35667367fef24b19f43fdada85e77713cc26743
Egor Glyakov <eglyakov@gmail.com>
2020-10-04 10:56:32 +0300

    1

---
+++ precache-manifest.67793e71a2605c0688bf11bae6e0c634.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b7a66f08267f2a3a4fa919b874fd8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f540138d33d01660ac9e",
+    "url": "/static/css/main.6507d2b8.chunk.css"
+  },
+  {
+    "revision": "a314a3303d784a4f7aff",
+    "url": "/static/js/2.0548d59f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0548d59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f540138d33d01660ac9e",
+    "url": "/static/js/main.24bc6bf8.chunk.js"
+  },
+  {
+    "revision": "acc281bf7b9101b1c74a",
+    "url": "/static/js/runtime-main.34d66920.js"
+  },
+  {
+    "revision": "dc9ea16138029a2d1548e821468bec4d",
+    "url": "/static/media/Kyzov.dc9ea161.jpg"
+  },
+  {
+    "revision": "d11eb109b888b6ceb301f7ef07d3f459",
+    "url": "/static/media/No.d11eb109.jpg"
+  },
+  {
+    "revision": "f3d6ce8404235da3fe0ade8a88c2c5b4",
+    "url": "/static/media/Yes.f3d6ce84.jpg"
+  },
+  {
+    "revision": "6f0f9eb896054fd3738cfe47230b6708",
+    "url": "/static/media/fon2.6f0f9eb8.jpg"
+  },
+  {
+    "revision": "ef3d9b1b8f1a009dd06886024ffe2164",
+    "url": "/static/media/imgSorev.ef3d9b1b.jpg"
+  },
+  {
+    "revision": "6768fd72bfed3408fdf4ad4e7b26f089",
+    "url": "/static/media/logo.6768fd72.png"
+  },
+  {
+    "revision": "630a450d1755f470a41aed238aadeb0c",
+    "url": "/static/media/strelka.630a450d.png"
+  }
+]);
